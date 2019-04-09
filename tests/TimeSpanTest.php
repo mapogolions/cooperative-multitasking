fb91b536fd9102e722c17506db648af7d5f7cb60
Pavel Ivanov <ivanovpavelalex45@gmail.com>
2019-04-09 13:30:34 +0500

    Added tests for \System\TimeSpan

---
+++ tests/TimeSpanTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Mapogolions\Multitask\Scheduler;
+use Mapogolions\Multitask\Suspendable\DataProducer;
+use Mapogolions\Multitask\System\TimeSpan;
+use Mapogolions\Multitask\Spies\Storage;
+use Mapogolions\Multitask\System\SystemCall;
+
+class TimeSpanTest extends TestCase
+{
+  public function testH()
+  {
+    $suspendable = (function () {
+      yield "start";
+      yield new TimeSpan(0.1);
+      yield "end";
+    })();
+    $spy = new Storage();
+    Scheduler::create()
+      ->spawn(new DataProducer($suspendable, $spy))
+      ->launch();
+    
+    $this->assertEquals(
+      ["start", "<system call> TimeSpan(0.1)", "end"], 
+      array_map(function ($it) {
+        return $it instanceof SystemCall ? (string) $it : $it;
+      }, $spy->stock())
+    );
+  }
+}
